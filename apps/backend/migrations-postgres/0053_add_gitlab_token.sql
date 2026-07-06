d9ea35a3bdd459f25787ff1dd2dc47d1a22359d0
Christophe Blefari <christophe.blefari@gmail.com>
2026-07-06 08:59:25 +0200

    Remove migration pre main merge

--- apps/backend/migrations-postgres/0053_add_gitlab_token.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "context_recommendation_config" ADD COLUMN "repo_provider" text CHECK ("repo_provider" IN ('github', 'gitlab'));--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "gitlab_access_token" text;
--- apps/backend/migrations-sqlite/0053_add_gitlab_token.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `context_recommendation_config` ADD `repo_provider` text;--> statement-breakpoint
-ALTER TABLE `user` ADD `gitlab_access_token` text;
